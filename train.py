718ec6ddc876eb79fd3359746d055c3691b6a593
tarepan <tarepan5884@gmail.com>
2023-05-18 05:37:17 +0000

    Refactor trainer

--- train.py
+++ train.py
@@ -3,19 +3,16 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import itertools
 import os
 import time
-import argparse
 import json
 import torch
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DistributedSampler, DataLoader
-import torch.multiprocessing as mp
-from torch.distributed import init_process_group
-from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import DataLoader
 from dataset import Dataset, mel_spectrogram, amp_pha_specturm, get_dataset_filelist
 from models import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss,\
     discriminator_loss, amplitude_loss, phase_loss, STFT_consistency_loss
 from utils import AttrDict, build_env, plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
+
 
 torch.backends.cudnn.benchmark = True
 
@@ -23,26 +20,26 @@ torch.backends.cudnn.benchmark = True
 def train(h):
 
     torch.cuda.manual_seed(h.seed)
-    device = torch.device('cuda:{:d}'.format(0))
+    device = torch.device('cuda:0')
 
-    generator = Generator(h).to(device)
-    mpd = MultiPeriodDiscriminator().to(device)
-    msd = MultiScaleDiscriminator().to(device)
-
+    # Model
+    generator =               Generator(h).to(device)
+    mpd       = MultiPeriodDiscriminator().to(device)
+    msd       =  MultiScaleDiscriminator().to(device)
     print(generator)
+
+    # State
     os.makedirs(h.checkpoint_path, exist_ok=True)
     print("checkpoints directory : ", h.checkpoint_path)
-
     if os.path.isdir(h.checkpoint_path):
-        cp_g = scan_checkpoint(h.checkpoint_path, 'g_')
+        cp_g  = scan_checkpoint(h.checkpoint_path, 'g_')
         cp_do = scan_checkpoint(h.checkpoint_path, 'do_')
-
     steps = 0
     if cp_g is None or cp_do is None:
         state_dict_do = None
         last_epoch = -1
     else:
-        state_dict_g = load_checkpoint(cp_g, device)
+        state_dict_g  = load_checkpoint(cp_g,  device)
         state_dict_do = load_checkpoint(cp_do, device)
         generator.load_state_dict(state_dict_g['generator'])
         mpd.load_state_dict(state_dict_do['mpd'])
@@ -50,38 +47,25 @@ def train(h):
         steps = state_dict_do['steps'] + 1
         last_epoch = state_dict_do['epoch']
 
-    optim_g = torch.optim.AdamW(generator.parameters(), h.learning_rate, betas=[h.adam_b1, h.adam_b2])
-    optim_d = torch.optim.AdamW(itertools.chain(msd.parameters(), mpd.parameters()),
-                                h.learning_rate, betas=[h.adam_b1, h.adam_b2])
-
+    # Optim/Sched
+    optim_g = torch.optim.AdamW(generator.parameters(),                              h.learning_rate, betas=[h.adam_b1, h.adam_b2])
+    optim_d = torch.optim.AdamW(itertools.chain(msd.parameters(), mpd.parameters()), h.learning_rate, betas=[h.adam_b1, h.adam_b2])
     if state_dict_do is not None:
         optim_g.load_state_dict(state_dict_do['optim_g'])
         optim_d.load_state_dict(state_dict_do['optim_d'])
-
     scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=h.lr_decay, last_epoch=last_epoch)
     scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=h.lr_decay, last_epoch=last_epoch)
 
+    # Data
     training_filelist, validation_filelist = get_dataset_filelist(h.input_training_wav_list, h.input_validation_wav_list)
+    trainset = Dataset(training_filelist,
+        h.segment_size, h.n_fft, h.num_mels, h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax,               n_cache_reuse=0, shuffle=True)
+    validset = Dataset(validation_filelist,
+        h.segment_size, h.n_fft, h.num_mels, h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, False, False, n_cache_reuse=0)
+    train_loader      = DataLoader(trainset, num_workers=h.num_workers, shuffle=False, batch_size=h.batch_size, pin_memory=True, drop_last=True)
+    validation_loader = DataLoader(validset, num_workers=1,             shuffle=False, batch_size=1,            pin_memory=True, drop_last=True)
 
-    trainset = Dataset(training_filelist, h.segment_size, h.n_fft, h.num_mels,
-                       h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, n_cache_reuse=0,
-                       shuffle=True, device=device)
-
-    train_loader = DataLoader(trainset, num_workers=h.num_workers, shuffle=False,
-                              sampler=None,
-                              batch_size=h.batch_size,
-                              pin_memory=True,
-                              drop_last=True)
-
-    validset = Dataset(validation_filelist, h.segment_size, h.n_fft, h.num_mels,
-                       h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, False, False, n_cache_reuse=0,
-                       device=device)
-    validation_loader = DataLoader(validset, num_workers=1, shuffle=False,
-                                   sampler=None,
-                                   batch_size=1,
-                                   pin_memory=True,
-                                   drop_last=True)
-
+    # Logger
     sw = SummaryWriter(os.path.join(h.checkpoint_path, 'logs'))
 
     generator.train()
@@ -89,73 +73,80 @@ def train(h):
     msd.train()
 
     for epoch in range(max(0, last_epoch), h.training_epochs):
+        #### Epoch ##############################################################################################################################
 
         start = time.time()
-        print("Epoch: {}".format(epoch+1))
+        print(f"Epoch: {epoch+1}")
 
-        for i, batch in enumerate(train_loader):
+        for batch in train_loader:
+            #### Step #######################################################################################################################
             start_b = time.time()
+
+            # Data
             x, logamp, pha, rea, imag, y = batch
-            x = torch.autograd.Variable(x.to(device, non_blocking=True))
-            y = torch.autograd.Variable(y.to(device, non_blocking=True))
-            logamp = torch.autograd.Variable(logamp.to(device, non_blocking=True))
-            pha = torch.autograd.Variable(pha.to(device, non_blocking=True))
-            rea = torch.autograd.Variable(rea.to(device, non_blocking=True))
-            imag = torch.autograd.Variable(imag.to(device, non_blocking=True))
+            x      = x.to(     device, non_blocking=True) # (maybe) Melspec
+            y      = y.to(     device, non_blocking=True) # (maybe) Gound-truth Waveform
+            logamp = logamp.to(device, non_blocking=True)
+            pha    = pha.to(   device, non_blocking=True)
+            rea    = rea.to(   device, non_blocking=True)
+            imag   = imag.to(  device, non_blocking=True)
+
+            # Reshape
             y = y.unsqueeze(1)
 
+            # Common_Forward
             logamp_g, pha_g, rea_g, imag_g, y_g = generator(x)
-            y_g_mel = mel_spectrogram(y_g.squeeze(1), h.n_fft, h.num_mels, h.sampling_rate, h.hop_size, h.win_size,
-                                      h.fmin, h.fmax)
 
             optim_d.zero_grad()
-
+            # D_Forward
             y_df_hat_r, y_df_hat_g, _, _ = mpd(y, y_g.detach())
-            loss_disc_f, losses_disc_f_r, losses_disc_f_g = discriminator_loss(y_df_hat_r, y_df_hat_g)
-
             y_ds_hat_r, y_ds_hat_g, _, _ = msd(y, y_g.detach())
-            loss_disc_s, losses_disc_s_r, losses_disc_s_g = discriminator_loss(y_ds_hat_r, y_ds_hat_g)
-
+            # D_Loss
+            loss_disc_f, _, _ = discriminator_loss(y_df_hat_r, y_df_hat_g)
+            loss_disc_s, _, _ = discriminator_loss(y_ds_hat_r, y_ds_hat_g)
             L_D = loss_disc_s + loss_disc_f
-
+            # D_Backward/Optim
             L_D.backward()
             optim_d.step()
 
             # Generator
             optim_g.zero_grad()
 
-            # Losses defined on log amplitude spectra
+            # G_Loss
+            ## Losses defined on log amplitude spectra
             L_A = amplitude_loss(logamp, logamp_g)
-
+            ## Losses defined on phase spectra
             L_IP, L_GD, L_PTD = phase_loss(pha, pha_g, h.n_fft, pha.size()[-1])
-            # Losses defined on phase spectra
             L_P = L_IP + L_GD + L_PTD
-
+            ## Losses defined on reconstructed STFT spectra
             _, _, rea_g_final, imag_g_final = amp_pha_specturm(y_g.squeeze(1), h.n_fft, h.hop_size, h.win_size)
             L_C = STFT_consistency_loss(rea_g, rea_g_final, imag_g, imag_g_final)
             L_R = F.l1_loss(rea, rea_g)
             L_I = F.l1_loss(imag, imag_g)
-            # Losses defined on reconstructed STFT spectra
             L_S = L_C + 2.25 * (L_R + L_I)
-
-            y_df_r, y_df_g, fmap_f_r, fmap_f_g = mpd(y, y_g)
-            y_ds_r, y_ds_g, fmap_s_r, fmap_s_g = msd(y, y_g)
+            ## Discriminator loss
+            _, y_df_g, fmap_f_r, fmap_f_g = mpd(y, y_g)
+            _, y_ds_g, fmap_s_r, fmap_s_g = msd(y, y_g)
+            ### Feature matching loss
             loss_fm_f = feature_loss(fmap_f_r, fmap_f_g)
             loss_fm_s = feature_loss(fmap_s_r, fmap_s_g)
-            loss_gen_f, losses_gen_f = generator_loss(y_df_g)
-            loss_gen_s, losses_gen_s = generator_loss(y_ds_g)
-            L_GAN_G = loss_gen_s + loss_gen_f
             L_FM = loss_fm_s + loss_fm_f
+            ### Adversarial loss
+            loss_gen_f, _ = generator_loss(y_df_g)
+            loss_gen_s, _ = generator_loss(y_ds_g)
+            L_GAN_G = loss_gen_s + loss_gen_f
+            ## Mel loss
+            y_g_mel = mel_spectrogram(y_g.squeeze(1), h.n_fft, h.num_mels, h.sampling_rate, h.hop_size, h.win_size, h.fmin, h.fmax)
             L_Mel = F.l1_loss(x, y_g_mel)
             # Losses defined on final waveforms
             L_W = L_GAN_G + L_FM + 45 * L_Mel
 
             L_G = 45 * L_A + 100 * L_P + 20 * L_S + L_W
-
+            # G_Backward/Optim
             L_G.backward()
             optim_g.step()
 
-            # STDOUT logging
+            # Logging
             if steps % h.stdout_interval == 0:
                 with torch.no_grad():
                     A_error = amplitude_loss(logamp, logamp_g).item()
@@ -255,11 +246,13 @@ def train(h):
                 generator.train()
 
             steps += 1
+            #### /Step ######################################################################################################################
 
         scheduler_g.step()
         scheduler_d.step()
-        
-        print('Time taken for epoch {} is {} sec\n'.format(epoch + 1, int(time.time() - start)))
+
+        print(f'Time taken for epoch {epoch + 1} is {int(time.time() - start)} sec\n')
+        #### /Epoch #############################################################################################################################
 
 
 def main():
@@ -277,8 +270,6 @@ def main():
     torch.manual_seed(h.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(h.seed)
-    else:
-        pass
 
     train(h)
 
